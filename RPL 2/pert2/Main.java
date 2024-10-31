2fb83b8a0999ace3aa341fefb603d400ab2399bd
koutarou <79289903+ruthnissi@users.noreply.github.com>
2024-10-31 21:45:00 +0700

    Create Main.java

---
+++ RPL 2/pert2/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+User user = new User(); //pembentukan objek
+
+        user.login();
+        user.logout();
+        
+Buyer buyer = new Buyer(); //pembentukan objek
+ 
+        buyer.login();
+        buyer.logout();
+        
+Seller seller = new Seller();
+        seller.addProduct("Mie Ayam merah");       
+    }    
+}
