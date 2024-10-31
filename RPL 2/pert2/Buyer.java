f7243937f461b35ebb41f6d15f062043e95a408c
koutarou <79289903+ruthnissi@users.noreply.github.com>
2024-10-31 21:47:03 +0700

    Create Buyer.java

---
+++ RPL 2/pert2/Buyer.java
@@ -0,0 +1,8 @@
+public class Buyer extends User{
+    public void login(){
+                System.out.println("Buyer melakukan login");
+    }        
+        public void logout(){
+                System.out.println("Buyer melakukan logout");
+        }    
+}
