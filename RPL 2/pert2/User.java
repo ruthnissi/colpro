a37b2d7f90137df33faa6fff3f000134a4c25b37
koutarou <79289903+ruthnissi@users.noreply.github.com>
2024-10-31 21:46:18 +0700

    Create User.java

---
+++ RPL 2/pert2/User.java
@@ -0,0 +1,13 @@
+public class User {
+    String nama;
+    String email;
+    String npm;
+    
+    public void login(){
+                System.out.println("user melakukan login");
+        
+    }
+           public void logout(){
+               System.out.println("user melakukan logout");
+           }    
+}
