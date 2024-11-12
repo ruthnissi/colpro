f70caccac3a4230b40ca24049b21c62d4095e711
koutarou <79289903+ruthnissi@users.noreply.github.com>
2024-11-12 14:42:00 +0700

    Create ModelMahasiswa.java

---
+++ RPL 2/pert4/ModelMahasiswa.java
@@ -0,0 +1,41 @@
+package mahasiswa.model;
+
+import jakarta.persistence.*;
+/**
+ *
+ * @author akmal
+ */
+
+@Entity
+@Table(name = "mahasiswa")
+public class ModelMahasiswa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "npm", nullable = false, length = 8)
+    private String npm;
+
+    @Column(name = "nama", nullable = false, length = 50)
+    private String nama;
+
+    @Column(name = "semester")
+    private int semester;
+
+    @Column(name = "ipk")
+    private float ipk;
+    
+    public ModelMahasiswa() {
+
+    }
+    
+    public ModelMahasiswa(int id, String npm, String nama, int semester, float ipk) {
+        this.id = id;
+        this.npm = npm;
+        this.nama = nama;
+        this.semester = semester;
+        this.ipk = ipk;
+    }
+    
+}
