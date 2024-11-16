2416b1f3d0696abffaa5aeb3faa97166b9957330
koutarou <79289903+ruthnissi@users.noreply.github.com>
2024-11-16 17:41:56 +0700

    Update ModelMahasiswa.java

--- RPL 2/pert5/ModelMahasiswa.java
+++ RPL 2/pert5/ModelMahasiswa.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+// Bagian ini sama seperti pert4, ditambahkan repository
 package com.mahasiswa.model;
 
 import jakarta.persistence.*;
