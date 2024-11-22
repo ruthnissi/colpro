d9cc20f3b928005f97187258ee4bd425dfbfb08b
koutarou <79289903+ruthnissi@users.noreply.github.com>
2024-11-22 22:11:23 +0700

    Update mainclass.java

--- RPL 2/pert6/mainclass.java
+++ RPL 2/pert6/mainclass.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mahasiswa;
 
 import com.mahasiswa.controller.MahasiswaController;
