e455fa7b48b8017b4ebaa366d60069d81c0a62e1
koutarou <79289903+ruthnissi@users.noreply.github.com>
2024-11-22 20:25:13 +0700

    Create mainclass.java

---
+++ RPL 2/pert6/mainclass.java
@@ -0,0 +1,5 @@
+ @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // Implement this method if you need to execute logic after Spring application starts
+        // Otherwise, you can leave it as is.
+    }
