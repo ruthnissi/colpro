14d2e6bfe56090b46fe07f4d7246ba69f618c4c4
koutarou <79289903+ruthnissi@users.noreply.github.com>
2024-11-22 22:11:09 +0700

    Update mainclass.java

--- RPL 2/pert6/mainclass.java
+++ RPL 2/pert6/mainclass.java
@@ -1,5 +1,35 @@
- @Override
-    public void run(ApplicationArguments args) throws Exception {
-        // Implement this method if you need to execute logic after Spring application starts
-        // Otherwise, you can leave it as is.
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mahasiswa;
+
+import com.mahasiswa.controller.MahasiswaController;
+import com.mahasiswa.service.MahasiswaService;
+import com.mahasiswa.view.MahasiswaView;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+/**
+ *
+ * @author USER
+ */
+@SpringBootApplication
+public class Pert6 {
+    
+    @Autowired
+    private MahasiswaService mahasiswaService;
+    
+    public static void main(String[] args) {
+        System.setProperty("java.awt.headless", "false");
+        
+        ApplicationContext context = SpringApplication.run(Pert6.class, args);
+        
+        MahasiswaController controller = context.getBean(MahasiswaController.class);
+        MahasiswaView mahasiswaView = new MahasiswaView(controller);
+        mahasiswaView.setVisible(true);
     }
+}
