4d3009f4076564818699d9269a53d829805c574d
koutarou <79289903+ruthnissi@users.noreply.github.com>
2024-12-05 20:04:52 +0700

    Create appconfig.java

---
+++ RPL 2/ujian1/appconfig.java
@@ -0,0 +1,9 @@
+package com.projectujian.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.projectujian")
+public class AppConfig {
+}
